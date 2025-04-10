263eaafc20c6c5b8002d336b7139b39e0bef4798
Luisa27Marini <luisa27.marini@gmail.com>
2025-04-10 01:34:26 +0000

    super_trunfo

---
+++ super_trunfo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+    char estado;= A
+    char codigo;= A01
+    char nomeCidade;= SaoPaulo
+    int populacao;= 12325000
+    float area;= 1521.11 
+    float pib;= 699.28
+    int pontosTuristicos;= 50
+
+
+
+
+return 0;
+}
